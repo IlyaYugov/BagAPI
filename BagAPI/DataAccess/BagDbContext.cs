9d41941eb8e6fd1cdaae256f780f0a036738d8df
IIYugov <ilyaugov@yandex.ru>
2018-04-19 16:59:46 +0300

    Initial repository

---
+++ BagAPI/DataAccess/BagDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class BagDbContext : DbContext
+    {
+        public BagDbContext(DbContextOptions<BagDbContext> options)
+            : base(options)
+        { }
+
+        //public DbSet<Blog> Blogs { get; set; }
+        //public DbSet<Post> Posts { get; set; }
+    }
+}
