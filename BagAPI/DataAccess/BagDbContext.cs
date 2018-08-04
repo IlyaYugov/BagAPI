173ae59333d6f3bdf06185c89205b0603b751d1e
IIYugov <ilyaugov@yandex.ru>
2018-08-04 21:40:23 +0300

    Add coplex primary key for UserMessenger

--- BagAPI/DataAccess/BagDbContext.cs
+++ BagAPI/DataAccess/BagDbContext.cs
@@ -9,13 +9,24 @@ namespace DataAccess
             : base(options)
         { }
 
-        public DbSet<Bag> Bags { get; set; }
-        public DbSet<BagType> BagTypes { get; set; }
-        public DbSet<City> Cites { get; set; }
-        public DbSet<MessengerType> MessengerTypes { get; set; }
-        public DbSet<Request> Requests { get; set; }
-        public DbSet<RequestStatus> RequestStatuses { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<UserMessenger> UserMessengers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserMessenger>()
+                .HasKey(c => new
+                {
+                    c.UserId,
+                    c.MessengerTypeId
+                });
+        }
+
+        public DbSet<Bag> Bag { get; set; }
+        public DbSet<BagType> BagType { get; set; }
+        public DbSet<City> Cite { get; set; }
+        public DbSet<MessengerType> MessengerType { get; set; }
+        public DbSet<Request> Request { get; set; }
+        public DbSet<RequestStatus> RequestStatus { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<UserMessenger> UserMessenger { get; set; }
     }
 }
